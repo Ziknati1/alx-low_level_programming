0170ee2f654e15250a4af9eed3af5ae6915f3bb0
Ziknati <ikulihezekiah@gmail.com>
2023-03-10 12:34:49 -0800

    push

---
+++ 0x02-functions_nested_loops/printx10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	char j;
+	
+	while (i <= 10)
+	{
+		j = 'a';
+		while (j <= 'z');
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar(_\n");
+		i++;
+	}
--- 0x09-static_libraries/README.md
+++
@@ -1,1 +0,0 @@
-1. Create the static library libmy.a containing all the functions listed below:
